6523f092ce118bb4ea7b552c00e664189cedbe83
marat.shaidullin@gmail.com <marat.shaidullin@gmail.com>
2017-03-06 23:27:34 +0300

    added custom layout to scenario execution dialog

--- app/src/main/java/ru/marat/smarthome/app/task/AsyncTaskManager.java
+++ app/src/main/java/ru/marat/smarthome/app/task/AsyncTaskManager.java
@@ -1,9 +1,17 @@
 package ru.marat.smarthome.app.task;
 
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
+import android.widget.TextView;
 import java.util.Arrays;
 import java.util.LinkedList;
 import org.apache.log4j.Logger;
@@ -16,7 +24,7 @@ public final class AsyncTaskManager<Params> implements TaskProgressTracker, OnCa
 
   private Logger logger = ALogger.getLogger(AsyncTaskManager.class);
   private final OnTaskCompleteListener taskCompleteListener;
-  private final ProgressDialog progressDialog;
+  private ProgressDialog progressDialog;
   private Task asyncTask;
   private LinkedList<Task> taskQueue = new LinkedList<>();
   private Activity context;
@@ -26,9 +34,18 @@ public final class AsyncTaskManager<Params> implements TaskProgressTracker, OnCa
     // Save reference to complete listener (activity)
     this.taskCompleteListener = taskCompleteListener;
     // Setup progress dialog
+    initProgressDialog();
+  }
+
+  /**
+   * Initial setup of ProgressDialog
+   */
+  private void initProgressDialog() {
     progressDialog = new ProgressDialog(context);
-    progressDialog.setIndeterminate(true);
+    progressDialog.setIndeterminate(false);
+    progressDialog.setTitle(R.string.scenario_execution);
     progressDialog.setCancelable(true);
+    progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
     progressDialog.setOnCancelListener(this);
     progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel",
         new DialogInterface.OnClickListener() {
@@ -56,6 +73,7 @@ public final class AsyncTaskManager<Params> implements TaskProgressTracker, OnCa
     if (taskQueue.isEmpty()) {
       throw new EmptyTaskQueueException(context.getString(R.string.task_queue_empty_exception));
     }
+    progressDialog.setMax(taskQueue.size());
     executeTask(taskQueue.poll());
   }
 
@@ -76,10 +94,21 @@ public final class AsyncTaskManager<Params> implements TaskProgressTracker, OnCa
     // Show dialog if it wasn't shown yet or was removed on configuration (rotation) change
     if (!progressDialog.isShowing()) {
       progressDialog.show();
+      LayoutInflater inflater = context.getLayoutInflater();
+      FrameLayout fl = (FrameLayout) progressDialog.findViewById(android.R.id.custom);
+      fl.addView(inflater.inflate(R.layout.scnr_exec_dialog_view, null),
+          new LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+      TextView progressMessage = (TextView) progressDialog
+          .findViewById(R.id.scnr_exec_dialog_message);
+      progressMessage.setMovementMethod(new ScrollingMovementMethod());
     }
     // Show current message in progress dialog
     if (message instanceof String) {
-      progressDialog.setMessage(message.toString());
+      progressDialog.setProgress(progressDialog.getProgress() + 1);
+      TextView progressMessage = (TextView) progressDialog
+          .findViewById(R.id.scnr_exec_dialog_message);
+      progressMessage
+          .append((progressMessage.getText().length() == 0 ? "" : "\n") + message.toString());
     }
   }
 
